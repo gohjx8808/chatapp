ffaf7144eb3e3e67df314a5e70287a49fee4a15c
Jing Xuan Goh <jingxuangoh@Jings-Air.realtek>
2021-04-10 17:30:30 +0800

    login template

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import Navigator from './src/Navigator';
+
+declare global {
+  namespace ReactNativePaper {
+    interface ThemeColors {
+      primary: string;
+    }
+  }
+}
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#0f4c81',
+  },
+};
 
 const App = () => {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <Navigator />
     </PaperProvider>
   );
--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -1,13 +1,82 @@
-import {View} from 'react-native';
-import React from 'react';
-import {Text} from 'react-native-paper';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Button, Card, TextInput, useTheme} from 'react-native-paper';
 
 const LoginScreen = () => {
+  const {colors} = useTheme();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [secure, setSecure] = useState(true);
+
   return (
-    <View>
-      <Text>hiiiiii</Text>
+    <View style={styles.backgroundView}>
+      <Card style={styles.loginCard}>
+        <Card.Title
+          title="Welcome to ChatApp!"
+          titleStyle={styles.loginTitle}
+        />
+        <Card.Content>
+          <TextInput
+            mode="outlined"
+            label="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+            theme={{colors: {primary: colors.primary}}}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            value={password}
+            onChangeText={text => setPassword(text)}
+            theme={{colors: {primary: colors.primary}}}
+            style={styles.topSpace}
+            secureTextEntry={secure}
+            right={
+              <TextInput.Icon
+                name={secure ? 'eye-off' : 'eye'}
+                onPress={() => setSecure(!secure)}
+              />
+            }
+          />
+          <View style={styles.buttonContainer}>
+            <Button
+              mode="contained"
+              onPress={() => console.log('login')}
+              style={styles.loginButton}
+              color="blue">
+              Log In
+            </Button>
+          </View>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  backgroundView: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginCard: {
+    height: '60%',
+    width: '80%',
+  },
+  loginTitle: {
+    textAlign: 'center',
+  },
+  topSpace: {
+    marginTop: 20,
+  },
+  loginButton: {
+    width: '50%',
+    alignSelf: 'center',
+  },
+  buttonContainer: {
+    marginVertical: '10%',
+  },
+});
