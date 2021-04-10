498e04a705b20f9752011a1894775b14363ab2ed
Jing Xuan Goh <jingxuangoh@Jings-Air.realtek>
2021-04-10 12:15:57 +0800

    integrate react navigation

---
+++ src/modules/navigation/views/Navigator.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+};
+
+export default Navigator;
