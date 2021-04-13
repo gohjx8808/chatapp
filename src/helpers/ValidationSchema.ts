1d49f187eb8ae2b01f2eb9d741fc5fe2c6bc6f69
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-04-13 23:09:04 +0800

    added strong password rgex validation

--- src/helpers/ValidationSchema.ts
+++ src/helpers/ValidationSchema.ts
@@ -7,7 +7,13 @@ export const LoginSchema = yup.object().shape({
 
 export const RegisterSchema = yup.object().shape({
   email: yup.string().email('Invalid email').required('Email is required'),
-  password: yup.string().required('Password is required'),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.{8,})(?=.+[!@#$%^&*()\-_=+{};:,<.>])(?=.+\d)(?=.+[a-z])(?=.+[A-Z]).*$/,
+      'Password is not strong enough',
+    )
+    .required('Password is required'),
   confirmPassword: yup
     .string()
     .required('Confirm Password is required')
