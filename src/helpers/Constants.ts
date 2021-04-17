851ea46b82d38ffebedb623eed8a5a31d5bf2bdc
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-04-17 16:59:21 +0800

    setup dialogflow service account

---
+++ src/helpers/Constants.ts
@@ -0,0 +1,5 @@
+import Config from 'react-native-config';
+
+export const dialogFlowClientEmail = Config.DIALOG_FLOW_CLIENT_EMAIL;
+export const dialogFlowPrivateKey = Config.DIALOG_FLOW_PRIVATE_KEY;
+export const dialogFlowProjectID = Config.DIALOG_FLOW_PROJECT_ID;
