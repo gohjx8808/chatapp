f51542160480d72f592c8e9bd0244d50ac0d4a8c
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-04-17 13:55:34 +0800

    customized gifted chat

--- src/helpers/Assets.ts
+++ src/helpers/Assets.ts
@@ -1,3 +1,4 @@
 export default {
   corgiSquare: require('../assets/corgi_square.jpeg'),
+  chatBg: require('../assets/chatBg.jpg'),
 };
--- src/modules/chat/views/ChatScreen.tsx
+++ src/modules/chat/views/ChatScreen.tsx
@@ -1,5 +1,18 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {GiftedChat, IMessage} from 'react-native-gifted-chat';
+import {ImageBackground, StyleSheet} from 'react-native';
+import {
+  Bubble,
+  BubbleProps,
+  Day,
+  DayProps,
+  GiftedChat,
+  IMessage,
+  InputToolbar,
+  InputToolbarProps,
+  Time,
+  TimeProps,
+} from 'react-native-gifted-chat';
+import Assets from '../../../helpers/Assets';
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
@@ -8,11 +21,12 @@ const ChatScreen = () => {
     setMessages([
       {
         _id: 1,
-        text: 'Hello developer',
+        text:
+          'Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?',
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'React Native',
+          name: 'FAQ Bot',
           avatar: 'https://placeimg.com/140/140/any',
         },
       },
@@ -25,15 +39,92 @@ const ChatScreen = () => {
     );
   }, []);
 
+  const renderCustomDay = (props: DayProps<IMessage>) => {
+    return (
+      <Day
+        {...props}
+        wrapperStyle={styles.dateWrapper}
+        textStyle={styles.dateText}
+      />
+    );
+  };
+
+  const renderCustomBubble = (props: BubbleProps<IMessage>) => {
+    return (
+      <Bubble
+        {...props}
+        textStyle={{
+          right: {color: 'black'},
+          left: {color: 'black'},
+        }}
+        wrapperStyle={{
+          right: {backgroundColor: '#D7ECD9'}, //green
+          left: {
+            backgroundColor: 'white',
+          },
+        }}
+      />
+    );
+  };
+
+  const renderCustomTime = (props: TimeProps<IMessage>) => {
+    return (
+      <Time
+        {...props}
+        timeTextStyle={{
+          right: {
+            color: '#9E9E9E',
+          },
+          left: {
+            color: '#9E9E9E',
+          },
+        }}
+      />
+    );
+  };
+
+  const renderCustomInputToolbar = (props: InputToolbarProps) => {
+    return (
+      <InputToolbar {...props} containerStyle={styles.textInputContainer} />
+    );
+  };
+
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={message => onSend(message)}
-      user={{
-        _id: 1,
-      }}
-    />
+    <ImageBackground source={Assets.chatBg} style={styles.chatBg}>
+      <GiftedChat
+        messages={messages}
+        onSend={message => onSend(message)}
+        user={{
+          _id: 1,
+          name: 'yaaaa',
+        }}
+        renderDay={renderCustomDay}
+        renderBubble={renderCustomBubble}
+        renderTime={renderCustomTime}
+        renderInputToolbar={renderCustomInputToolbar}
+        placeholder="Type a message"
+      />
+    </ImageBackground>
   );
 };
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+  chatBg: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  dateWrapper: {
+    backgroundColor: 'white',
+    width: '25%',
+    borderRadius: 20,
+    paddingVertical: 3,
+  },
+  dateText: {
+    textAlign: 'center',
+  },
+  textInputContainer: {
+    borderRadius: 10,
+  },
+});
